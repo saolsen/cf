f30d1b773e282d1474294ed799da92a3aeebd228
steve olsen <steve@steve.computer>
2025-05-02 12:41:20 -0500

    template file

---
+++ lib/template.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char buf[1024];
+
+int main(void) {
+#ifndef ONLINE_JUDGE
+  freopen("0.in", "r", stdin);
+#endif
+  char *line = fgets(buf, sizeof(buf) / sizeof(buf[0]), stdin);
+  assert(line != NULL);
+  fprintf(stderr, "%s", line);
+  long n = strtol(line, &line, 10);
+  long m = strtol(line, &line, 10);
+  fprintf(stderr, "%ld %ld\n", n, m);
+  return 0;
+}
